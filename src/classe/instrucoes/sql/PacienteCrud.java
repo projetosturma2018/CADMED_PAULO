f0aefe495ab2ef937b4caae70be04d4183942d58
PauloGouveia <paulo_adr2010@hotmail.com>
2018-03-10 12:29:03 -0300

    subindo para uma nova plataforma git todas as classes e arquivo sql

---
+++ src/classe/instrucoes/sql/PacienteCrud.java
@@ -0,0 +1,112 @@
+package classe.instrucoes.sql;
+
+import classe.modell.Conexao;
+import java.sql.Connection;
+import classe.modell.PacienteClinica;
+ 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PacienteCrud {
+
+    Conexao conexao = new Conexao();
+    Connection conex = conexao.criarConexao();
+
+    public int salvarUsuario(PacienteClinica paciente) {
+        String sqlInsert = "insert"
+                + "into"
+                + "tb_paciente(nome, cpf, endereco, rg, sexo, telefone, medico_responsavel)"
+                + "values(?,?,?,?,?,?,?)";
+
+        try {
+
+            PreparedStatement preparacaoSalvar = conex.prepareStatement(sqlInsert);
+            preparacaoSalvar.setString(1, paciente.getNome());
+            preparacaoSalvar.setString(2, paciente.getCpf());
+            preparacaoSalvar.setString(3, paciente.getEndereco());
+            preparacaoSalvar.setString(4, paciente.getRg());
+            preparacaoSalvar.setString(5, paciente.getSexo());
+            preparacaoSalvar.setString(6, paciente.getTelefone());
+            preparacaoSalvar.setString(7, paciente.getMedicoResponsavel());
+        } catch (SQLException ex) {
+            Logger.getLogger(PacienteCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
+    }
+
+    public List<PacienteClinica> listarPaciente() {
+        String sqlSelect = "select"
+                +"from tb_paciente";
+        List<PacienteClinica> listaDePaciente = new ArrayList<PacienteClinica>();
+        try {
+
+            PreparedStatement preparacaoSelect = conex.prepareStatement(sqlSelect);
+            ResultSet resultadoLista = preparacaoSelect.executeQuery();
+
+            while (resultadoLista.next()) {
+                PacienteClinica paciente = new PacienteClinica();
+                paciente.setIdPaciente(resultadoLista.getInt("id_paciente"));
+                paciente.setNome(resultadoLista.getString("nome"));
+                paciente.setCpf(resultadoLista.getString("cpf"));
+                paciente.setEndereco(resultadoLista.getString("endereco"));
+                paciente.setRg(resultadoLista.getString("rg"));
+                paciente.setSexo(resultadoLista.getString("sexo"));
+                paciente.setTelefone(resultadoLista.getString("telefone"));
+                paciente.setMedicoResponsavel(resultadoLista.getString("medico_responsavel"));
+                paciente.setFkConvenio(resultadoLista.getInt("id_convenio"));
+                paciente.setFkconsulta(resultadoLista.getInt("id_consulta"));
+                
+                listaDePaciente.add(paciente);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(PacienteCrud.class.getName()).log(Level.SEVERE, null, ex);
+
+            return null;
+
+        }
+        return listaDePaciente;
+    }
+    
+        public void atualizarPerfil(PacienteClinica paciente) {
+        String sqlUpdate = "update tb_paciente "
+                + "set nome = ?, cpf = ?, endereco = ?, rg = ?, sexo = ?, telefone = ?, medico_responsavel = ? "
+                + "where id_paciente = ? ";
+        try {
+
+            PreparedStatement preparacaoUpdate = conex.prepareStatement(sqlUpdate);
+            preparacaoUpdate.setString(1,paciente.getNome());
+            preparacaoUpdate.setString(2,paciente.getCpf());
+            preparacaoUpdate.setString(3,paciente.getEndereco());
+            preparacaoUpdate.setString(4,paciente.getRg());
+            preparacaoUpdate.setString(5,paciente.getSexo());
+            preparacaoUpdate.setString(6,paciente.getTelefone());
+            preparacaoUpdate.setString(7,paciente.getMedicoResponsavel());
+            preparacaoUpdate.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(PacienteCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void ExcluirPerfil(PerfilUsuario perfil){
+       String sqlDelete = "delete from tb_perfil"
+                    + " where codigo_perfil = ?";
+ 
+        try {
+          PreparedStatement preparacaoDelete = conexao.prepareStatement(sqlDelete);
+          preparacaoDelete.setInt(1,perfil.getCodigoPerfil());
+          preparacaoDelete.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoPerfilUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    
+    }
+
+
---
+++ src/testes/TesteConexao.java
@@ -0,0 +1,20 @@
+
+package testes;
+
+import classe.modell.Conexao;
+
+/**
+ *
+ * @author PauloGouveia
+ */
+public class TesteConexao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Conexao con = new Conexao();
+       con.criarConexao();
+    }
+    
+}
